5e81be15419204145a930daa2a91af7f62fd1ca3
yedu <kacper.wiecek@student.wsb.edu.pl>
2025-05-23 20:44:47 +0200

    dodanie kodu z poprzedniego projektu

--- Program.cs
+++ Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+class FoodItem
+{
+    public string Name { get; set; }
+    public int Calories { get; set; }
+
+    public FoodItem(string name, int calories)
+    {
+        Name = name;
+        Calories = calories;
+    }
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Jedzenie: {Name}, Kalorie: {Calories} kcal");
+    }
+}
+class Meal
+{
+    public string Name { get; set; }
+    public List<FoodItem> FoodItems { get; set; }
+
+    public Meal(string name)
+    {
+        Name = name;
+        FoodItems = new List<FoodItem>();
+    }
+
+    public void AddFood(FoodItem food)
+    {
+        FoodItems.Add(food);
+    }
+
+    public int GetTotalCalories()
+    {
+        int total = 0;
+        foreach (var food in FoodItems)
+        {
+            total += food.Calories;
+        }
+        return total;
+    }
+
+    public void DisplayMealInfo()
+    {
+        Console.WriteLine($"Posiłek: {Name}");
+        Console.WriteLine("Składniki:");
+        foreach (var food in FoodItems)
+        {
+            food.DisplayInfo();
+        }
+        Console.WriteLine($"Łącznie: {GetTotalCalories()} kcal\n");
+    }
+}
